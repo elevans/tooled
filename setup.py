61551ae9602391ea2b6a83fd895162ecf73374c7
Edward Evans <elevans2@wisc.edu>
2021-09-27 09:47:17 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='tooled',
+        version='0.0.1.dev0',
+        author='Edward Evans',
+        author_email='elevans2@wisc.edu',
+        packages=find_packages(),
+        platforms=['any'],
+        description='A collection of useful tools.',
+        )
+
