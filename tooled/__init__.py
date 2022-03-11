55eebc840d3ce527bb64d72704b0b66c07a7d9db
Edward Evans <elevans2@wisc.edu>
2022-03-11 11:17:43 -0600

    Update obj_info to print columns

--- tooled/__init__.py
+++ tooled/__init__.py
@@ -1,6 +1,11 @@
-def dir_list(object):
-    output = dir(object)
-    for item in output:
-        print(item)
+def obj_info(object):
+    """
+    Print out a readable list from dir of an object.
+    """
+    print(f"\nObject: {object}\nType: {type(object)}\n")
+
+    obj_dir = dir(object)
+    for at1, at2, at3 in zip(obj_dir[::3], obj_dir[1::3], obj_dir[2::3]):
+        print("{:<30}{:<30}{:<}".format(at1, at2, at3))
 
     return
