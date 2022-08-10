3724bcef6e6159ee0a599e887ed6ce16d802bfa6
Edward Evans <elevans2@wisc.edu>
2022-08-10 12:11:02 -0500

    Remove find_packages()

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-from setuptools import setup, find_packages
-
+from setuptools import setup
 setup(
-        name='tooled',
-        version='0.0.1.dev0',
-        author='Edward Evans',
-        author_email='elevans2@wisc.edu',
-        packages=find_packages(),
-        platforms=['any'],
-        description='A collection of useful tools.',
-        )
-
+    name="tooled",
+    version="0.1.0.dev0",
+    author="Edward Evans",
+    author_email="elevans2@wisc.edu",
+    platforms=["any"],
+    description="A collection of useful tools.",
+)
